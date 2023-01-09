ff97c475148e23c538dad0d734acf33cbe16439f
Dagg <daggintosh@outlook.com>
2023-01-08 21:37:04 -0800

    Property

--- getFox.js
+++ getFox.js
@@ -3,7 +3,7 @@ fetch("https://randomfox.ca/floof/")
     .then(res => res.json())
     .then(res => {
         img.src = res.image;
-        document.querySelector("meta[name='og:image']")
+        document.querySelector("meta[property='og:image']")
             .setAttribute("content", res.image);
     })
     .catch(err => console.error(err));
