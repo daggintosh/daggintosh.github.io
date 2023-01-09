0018e6b9e06b2a9cb96281e0233db1d597b98d3a
Dagg <daggintosh@outlook.com>
2023-01-08 21:35:06 -0800

    Metadata test 1

--- getFox.js
+++ getFox.js
@@ -1,5 +1,9 @@
 let img = document.getElementById("fox");
 fetch("https://randomfox.ca/floof/")
     .then(res => res.json())
-    .then(res => img.src = res.image)
+    .then(res => {
+        img.src = res.image;
+        document.querySelector("meta[name='og:image']")
+            .setAttribute("content", res.image);
+    })
     .catch(err => console.error(err));
