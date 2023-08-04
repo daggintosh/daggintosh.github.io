c8c6f8cc7d2f6a6ca00a09261ddc2bfaa63a35a4
Dagg <dagg@kyzune.com>
2023-08-04 12:53:13 -0700

    Add files via upload

--- getFox.js
+++
@@ -1,5 +0,0 @@
-let img = document.getElementById("fox");
-fetch("https://randomfox.ca/floof/")
-    .then(res => res.json())
-    .then(res => img.src = res.image)
-    .catch(err => console.error(err));
