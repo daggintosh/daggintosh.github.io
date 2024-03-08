6a1166195086d4dbdabab04550016380ea0e3e34
Dagg <dagg@kyzune.com>
2024-03-08 09:42:18 -0800

    Add refresh button to page, fix bug resulting in response not changing between reloads on WebKit

--- getFox.js
+++ getFox.js
@@ -1,19 +1,9 @@
 let img = document.getElementById("fox");
 img.addEventListener("load", () => img.style.animationPlayState = "running")
-fetch("https://randomfox.ca/floof/")
-    .then(res => res.json())
-    .then(res => img.src = res.image)
-    .catch(() => {
-        let container = document.getElementById("container")
-        let node1 = document.createElement("h1")
-        let node2 = document.createElement("p")
-        node1.textContent = "You're offline"
-        node1.style.animation = "fade 1s ease-out"
-        node2.textContent = "Connect to the internet to continue"
-        node2.style.animation = "fade 1s ease-out"
-        container.style.alignItems = "center"
-        container.style.justifyContent = "center"
-        container.style.flexDirection = "column"
-        container.append(node1, node2)
-        img.remove()
-    })
+
+function getFox() {
+    fetch("https://randomfox.ca/floof/", { cache: "no-store" })
+        .then(res => res.json())
+        .then(res => img.src = res.image)
+        .catch(err => console.error(err))
+}
