1e6a05979941000e3ea506f84e5f2a01a28a7569
Dagg <dagg@kyzune.com>
2024-03-08 13:50:49 -0800

    various tweaks

--- getFox.js
+++ getFox.js
@@ -1,6 +1,24 @@
 let img = document.getElementById("fox");
 img.addEventListener("load", () => img.style.animationPlayState = "running")
 
+let header = document.getElementById("header");
+let container = document.getElementById("container")
+
+addEventListener("resize", () => {
+    fixHeight()
+})
+
+function firstLoad() {
+    fixHeight()
+    getFox()
+}
+
+function fixHeight() {
+    let offset = (100 * header.offsetHeight) / window.innerHeight
+    container.style.height = `${100 - offset}vh`
+    console.log(header.offsetHeight);
+}
+
 function getFox() {
     fetch("https://randomfox.ca/floof/", { cache: "no-store" })
         .then(res => res.json())
